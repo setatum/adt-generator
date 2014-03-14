11502b51ad97740b73b7ecc0d4c9627fa3a5730d
Scott Tatum <scott.tatum@medseek.com>
2014-03-13 19:31:35 -0500

    Initial structure and pom.

---
+++ src/main/java/com/medseek/clinical/generator/adt/Application.java
@@ -0,0 +1,16 @@
+package com.medseek.clinical.generator.adt;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Entry point for the ADT generator application. Pulls in various builders
+ * (classes that generate random pieces of the ADT structure.
+ * @author setatum
+ *
+ */
+@Configuration
+@ComponentScan
+public class Application {
+
+}
